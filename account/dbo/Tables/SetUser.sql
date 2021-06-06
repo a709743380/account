056628243bad1e99f43aef0bfe1d2836628e863a
wwwhu <wwwhu@LAPTOP-UI9N7MK5>
2021-06-06 21:03:34 +0800

    更新

---
+++ account/dbo/Tables/SetUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SetUser] (
+    [id]      INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]  NVARCHAR (50) NULL,
+    [Gender]  NVARCHAR (50) NULL,
+    [Address] NVARCHAR (50) NULL,
+    [tel]     NVARCHAR (50) NULL,
+    CONSTRAINT [PK_SetUser] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
--- account/dbo/Tables/account.sql
+++ account/dbo/Tables/account.sql
@@ -1,11 +1,10 @@
 ﻿CREATE TABLE [dbo].[account] (
-    [id]      INT           IDENTITY (1, 1) NOT NULL,
-    [name]    NVARCHAR (50) NULL,
-    [userid]  NVARCHAR (50) NULL,
-    [passwd]  NVARCHAR (50) NULL,
-    [Email]   NVARCHAR (50) NULL,
-    [Gender]  NVARCHAR (50) NULL,
-    [Address] NVARCHAR (50) NULL,
+    [id]     INT           IDENTITY (1, 1) NOT NULL,
+    [name]   NVARCHAR (50) NULL,
+    [userid] NVARCHAR (50) NULL,
+    [passwd] NVARCHAR (50) NULL,
     CONSTRAINT [PK_account] PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+
 
